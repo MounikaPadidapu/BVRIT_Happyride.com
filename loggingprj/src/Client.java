37b50a65463dae7adefc7a177ef6c87e1fd029dd
SiriSudhathi <sirivsudhathi@gmail.com>
2016-06-07 13:56:07 +0530

    created logging files

---
+++ loggingprj/src/Client.java
@@ -0,0 +1,17 @@
+
+import org.apache.log4j.Logger;
+ 
+public class Client {
+ 
+  static Logger l = Logger.getLogger(Client.class.getName());
+ 
+  public static void main(String[] args) {      
+ 
+    
+      l.info("looged in successfully");
+   
+ 
+
+ 
+  }
+}
